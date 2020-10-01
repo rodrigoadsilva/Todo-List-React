97cfb8e459e212b512e6137489e790980d5d9583
Rodrigo da Silva <rodrigo@rodrigoads.com>
2020-10-01 20:40:05 -0300

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const App = () => {
+    return(
+        <div>
+            <Navbar/>
+            <h1>My first App</h1>
+            
+        </div>
+    );
+}
+
+export default App;
